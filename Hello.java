05d5c249f8bcb7b5e21d30cace16b6cc8b080751
AnshumanSachdeva <anshumansachdeva8@gmail.com>
2026-02-25 15:46:22 +0530

    First program in java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main (String[] agrs){
+        System.out.println("Hello World");
+    }
+}
+// main function ko toh humne class k andar banaya hai aur bina object k toh hum call hi nhi kar skte toh yeh call kaise ho raha hai? "static" keyword ki vjh se ho rha hai
